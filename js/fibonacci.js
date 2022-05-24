132270e6ca12fab07609a1755ecebc30cd17f0bf
colour-me-bibi <bennyforeman1@gmail.com>
2022-05-24 08:07:03 -0700

    Implement fibonacci functions

--- js/fibonacci.js
+++ js/fibonacci.js
@@ -1,5 +1,44 @@
-const fibonacci = (num) => {
-    
-}
+/**
+ * Returns the nth fibonacci number using recursion and a cache.
+ *
+ * @param {Integer} n The nth fibonacci number to return
+ * @returns The nth fibonacci number
+ */
+const rFibonacci = (n) => {
+  // implement lru cache
+  const cahce = {};
 
-module.exports = {fibonacci}
+  const fib = (n) => {
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+
+    if (cahce[n]) return cahce[n];
+
+    const result = fib(n - 1) + fib(n - 2);
+    cahce[n] = result;
+    return result;
+  };
+
+  return fib(n);
+};
+
+/**
+ * Returns the nth fibonacci number.
+ *
+ * @param {Integer} n
+ * @returns The nth fibonacci number
+ */
+const fibonacci = (n) => {
+  let a = 0,
+    b = 1;
+
+  for (let i = 0; i < n; i++) {
+    let temp = a;
+    a = b;
+    b = temp + b;
+  }
+
+  return a;
+};
+
+module.exports = { fibonacci };
--- python/fibonacci.py
+++ python/fibonacci.py
@@ -1,4 +1,40 @@
+
+
+from functools import lru_cache
+
+
+@lru_cache
+def r_fibonacci(n):
+    """Returns the nth term of the fibonacci sequence recursively using a cache.
+
+    Args:
+        n (int): The nth term of the fibonacci sequence.
+
+    Returns:
+        int: The nth term of the fibonacci sequence.
+    """
+
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+
+    return r_fibonacci(n - 1) + r_fibonacci(n - 2)
+
+
 def fibonacci(n):
-  pass
+    """Returns the nth term of the fibonacci sequence.
 
+    Args:
+        n (int): The nth term of the fibonacci sequence.
 
+    Returns:
+        int: The nth term of the fibonacci sequence.
+    """
+
+    a, b = 0, 1
+
+    for _ in range(n):
+        a, b = b, a + b
+
+    return a

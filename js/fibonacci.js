ea0ecd1aff44b89845e8e4a1be45c389c28a61a4
colour-me-bibi <bennyforeman1@gmail.com>
2022-05-24 12:14:29 -0700

    Fix formatting and cache check

--- js/fibonacci.js
+++ js/fibonacci.js
@@ -29,8 +29,8 @@ const rFibonacci = (n) => {
  * @returns The nth fibonacci number
  */
 const fibonacci = (n) => {
-  let a = 0,
-    b = 1;
+  let a = 0;
+  let b = 1;
 
   for (let i = 0; i < n; i++) {
     let temp = a;

abd9383bee04f6ac677287c8d3719522b46d5ad7
colour-me-bibi <bennyforeman1@gmail.com>
2022-05-25 07:33:55 -0700

    Fix off-by-one bug in fibonacci

--- js/fibonacci.js
+++ js/fibonacci.js
@@ -9,8 +9,8 @@ const rFibonacci = (n) => {
   const cahce = {};
 
   const fib = (n) => {
-    if (n == 0) return 0;
-    if (n == 1) return 1;
+    if (n == 1) return 0;
+    if (n == 2) return 1;
 
     if (cahce[n] !== undefined) return cahce[n];
 
